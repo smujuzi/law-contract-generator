05d54c70eab04faedd16b8849ce9804aa38b3e35
Stuart <stuartkasekende1@gmail.com>
2023-07-30 14:24:01 +0100

    post endpoint that generates contract

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from dynamo import get_contract_keywords
+from s3 import generate_contract
+import os
 # import gspread
 # from oauth2client.service_account import ServiceAccountCredentials
 
@@ -11,33 +13,36 @@ app = Flask(__name__)
 # gc = gspread.authorize(credentials)
 # spreadsheet_key = 'YOUR_SPREADSHEET_KEY'  # Replace this with the key of your Google Sheets document
 
-# @app.route('/submit_form', methods=['POST'])
-# def submit_form():
-#     try:
-#         data = request.json
-#         # Extract data from the POST request (assuming it's in JSON format)
-#         # Modify the following lines based on your form's questions
-#         name = data['name']
-#         email = data['email']
-#         message = data['message']
+# NOTE: This was in the post function
+# # Open the Google Sheets document and the desired worksheet
+        # worksheet = gc.open_by_key(spreadsheet_key).sheet1
 
-#         # Open the Google Sheets document and the desired worksheet
-#         worksheet = gc.open_by_key(spreadsheet_key).sheet1
+        # # Append the form data to the worksheet
+        # row = [name, email, message]
+        # worksheet.append_row(row)
 
-#         # Append the form data to the worksheet
-#         row = [name, email, message]
-#         worksheet.append_row(row)
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    try:
+        data = request.json
+        contract_link = generate_contract(data)
+        #Clean up
+        os.remove(data['contract_name'])
 
-#         return jsonify({"status": "success", "message": "Form submitted successfully!"})
-#     except Exception as e:
-#         return jsonify({"status": "error", "message": str(e)})
+        return jsonify(
+                {
+                    "status": "success", 
+                    "message": "Form submitted successfully!",
+                    "download_link": contract_link
+                }
+            )
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 @app.route('/get_keywords', methods=['GET'])
 def get_keywords():
     try:
         data = request.json
-        # Extract data from the POST request (assuming it's in JSON format)
-        # Modify the following lines based on your form's questions
         contract_name = data['contract_name']
         
         keywords = get_contract_keywords(contract_name)
